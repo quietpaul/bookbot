e186c09622058f33194c16f0d68f38331f543100
quietpaul <p.woodworth91@gmail.com>
2025-06-02 22:05:53 -0700

    D: cleaned up main.py. some progress on num_chars

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ def main():
     book = get_book_text("books/frankenstein.txt")
     word_count = num_words(book)
     print(f"{word_count} words found in the document")
+    print(chars(book))
 
 main()
