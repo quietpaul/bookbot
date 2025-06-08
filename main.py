dfdbf4723caae6cccf8508ee4ab3f8dee66a7348
quietpaul <p.woodworth91@gmail.com>
2025-06-08 00:47:34 -0700

    Started to automate book path workflow but got stuck will come back

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from stats import num_words, num_chars, get_book_text, sort_on, sort_list
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book = get_book_text(book_path)
     word_count = num_words(book)
     unsorted_dict = num_chars(book)
